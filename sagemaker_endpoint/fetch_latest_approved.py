d6265c06bbefbc4fd90ab9e2f6a9d32bfcac5192
Niklas Palm <niklas-palm@hotmail.com>
2024-09-18 15:24:39 +0200

    feat: add SM endpoint cfn and deployment

---
+++ sagemaker_endpoint/fetch_latest_approved.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import boto3
+
+
+def get_latest_approved_model_package(model_package_group):
+    client = boto3.client("sagemaker")
+    response = client.list_model_packages(
+        ModelPackageGroupName=model_package_group,
+        ModelApprovalStatus="Approved",
+        SortBy="CreationTime",
+        SortOrder="Descending",
+        MaxResults=1,
+    )
+
+    model_packages = response.get("ModelPackageSummaryList", [])
+    if not model_packages:
+        print("No approved model packages found.")
+        sys.exit(1)  # Exit with non-zero code to indicate failure
+
+    model_package_arn = model_packages[0]["ModelPackageArn"]
+    return model_package_arn
+
+
+if __name__ == "__main__":
+    model_package_group = os.getenv("MODEL_PACKAGE_GROUP")
+    if not model_package_group:
+        print("Model package group not specified.")
+        sys.exit(1)
+
+    arn = get_latest_approved_model_package(model_package_group)
+    print(arn)
